82d5e724f8a9b8aea6e6444d8a199aec7c4b0a9d
Adam Naamani <adamnaamani@gmail.com>
2018-12-16 14:22:59 -0800

    Testing framework

---
+++ src/components/Tests.spec.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai'
+import { mount } from '@vue/test-utils'
+import Datatable from './datatable/index.vue'
+
+describe('The only true wisdom is in knowing you know nothing.', () => {
+  it('true dat', () => {
+    expect(true).to.equal(true)
+  })
+})
