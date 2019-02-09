bd9c25256574498bcfefcc92a2fdc942d31b4cab
Adam Naamani <adamnaamani@gmail.com>
2019-02-08 19:57:47 -0800

    Datepicker

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import VueNumeric from 'vue-numeric'
 import axios from 'axios'
 import moment from 'moment'
 import ShardsVue from 'shards-vue'
-import Datatable from './components/datatable/index.vue'
 import { store } from './store'
 
 import 'bootstrap/dist/css/bootstrap.css'
@@ -12,6 +11,8 @@ import 'shards-ui/dist/css/shards.css'
 Vue.config.productionTip = false
 Vue.use(ShardsVue)
 Vue.use(VueNumeric)
+
+import Datatable from './components/datatable/index.vue'
 
 let div = document.createElement('div')
 div.id = 'datatable'
